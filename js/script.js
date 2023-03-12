5a8dc7827e08b02056d44593c831c5655a01351a
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-12 17:03:26 +0300

    refactor: refactor js, fix styles

--- js/script.js
+++ js/script.js
@@ -4,69 +4,78 @@ const resultsEl = document.querySelector(".results");
 
 searchform.addEventListener("submit", function (event) {
   event.preventDefault();
+
   let search = searchform.searchInput.value.trim();
+
   // валидация формы
   if (search.length < 3) {
     searchEl.classList.add("error");
     return;
   }
 
-  //запос репозиториев по поисковой строке
-  let repos = searchRepo(search);
-  repos.then((res) => {
-    console.log(res);
-    showRepos(res);
-  });
+  searchRepo(search, 10)
+    .then((repos) => showRepos(repos))
+    .catch((error) => showError(error));
 });
 
 searchform.addEventListener("input", function () {
   searchEl.classList.remove("error");
 });
 
-async function searchRepo(search, perPage = 10) {
+function searchRepo(search, perPage = 10) {
   const queryString =
     "?q=" + encodeURIComponent(search) + `&per_page=${perPage}`;
   let url = new URL(queryString, "https://api.github.com/search/repositories");
-  console.log(url);
-  let response = await fetch(url);
-  let json = await response.json();
-  return json;
+
+  return fetch(url).then((response) => {
+    return response.json();
+  });
 }
 
 function showRepos(repos) {
   clearResults();
+
   if (repos.items.length) {
     repos.items.forEach((repo) => {
-      console.dir(repo);
-      let repoEl = document.createElement("div");
-      repoEl.className = "repo";
-      let repoTitleEl = document.createElement("a");
-      repoTitleEl.className = "repo-title";
-      repoTitleEl.href = repo.html_url;
-      repoTitleEl.innerText = repo.name;
-      repoTitleEl.target = "_blank";
-      repoEl.append(repoTitleEl);
-      let repoDetailsEl = document.createElement("div");
-      repoDetailsEl.className = "repo-details";
-      repoEl.append(repoDetailsEl);
-      let repoAuthorEl = document.createElement("p");
-      repoAuthorEl.innerText = `Автор: ${repo.owner.login}`;
-      repoDetailsEl.append(repoAuthorEl);
-      let repoCreatedEl = document.createElement("p");
-      repoCreatedEl.innerText = `Создано: ${repo.created_at}`;
-      repoDetailsEl.append(repoCreatedEl);
-
-      resultsEl.append(repoEl);
+      resultsEl.append(createRepo(repo));
     });
   } else {
-    const noResultsTitle = document.createElement("h3");
-    noResultsTitle.innerText = "По вашему запросу ничего не найдено...";
-    resultsEl.prepend(noResultsTitle);
+    showText("По вашему запросу ничего не найдено...");
   }
+}
+
+function showText(text) {
+  clearResults();
+  const titleEl = document.createElement("h3");
+  titleEl.innerText = text;
+  resultsEl.append(titleEl);
 }
 
 function clearResults() {
   while (resultsEl.firstChild) {
     resultsEl.removeChild(resultsEl.firstChild);
   }
+}
+
+function createRepo(repo) {
+  let repoEl = document.createElement("div");
+  repoEl.className = "repo";
+  let repoTitleEl = document.createElement("a");
+  repoTitleEl.className = "repo-title";
+  repoTitleEl.href = repo.html_url;
+  repoTitleEl.innerText = repo.name;
+  repoTitleEl.target = "_blank";
+  repoEl.append(repoTitleEl);
+  let repoDetailsEl = document.createElement("div");
+  repoDetailsEl.className = "repo-details";
+  repoEl.append(repoDetailsEl);
+  let repoAuthorEl = document.createElement("p");
+  repoAuthorEl.innerHTML = `Автор: <b>${repo.owner.login}<b>`;
+  repoDetailsEl.append(repoAuthorEl);
+  let repoCreatedEl = document.createElement("p");
+  repoCreatedEl.innerText = `Создано: ${repo.created_at
+    .replace("T", ", ")
+    .replace("Z", "")}`;
+  repoDetailsEl.append(repoCreatedEl);
+  return repoEl;
 }
